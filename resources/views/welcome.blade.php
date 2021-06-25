570f667b1a930311360fef491c3195c863c253d1
shuvo <shuvoafjal@gmail.com>
2021-06-26 00:36:33 +0600

    Langing Page, Login, Register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,15 +4,21 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>Varatia-Vara(ভাড়াটিয়া-ভাড়া)</title>
+        <!-- Title Logo -->
+        <link rel="icon" href="/rent.png" type="image/gif/png">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
+        <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/caf4fd03c7.js" crossorigin="anonymous"></script>
+        <!-- Boostrap 4 -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #EDBB99;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
@@ -70,10 +76,10 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}"><button type="button" class="btn btn-success">Login</button></a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}"><button type="button" class="btn btn-primary">Register</button></a>
                         @endif
                     @endauth
                 </div>
@@ -81,20 +87,30 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <!-- Laravel -->
+                    <video  controls loop width="500" height="240" autoplay>
+                        <source src="welcome.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs"><button type="button" class="btn btn-info">About</button></a>
+                    <a href="https://laravel.com/docs"><button type="button" class="btn btn-info">Tutorial</button></a>
+                    <!-- <a href="https://laracasts.com">Laracasts</a> -->
+                    <!-- <a href="https://laravel-news.com">News</a> -->
+                    <!-- <a href="https://blog.laravel.com">Blog</a> -->
+                    <!-- <a href="https://nova.laravel.com">Nova</a> -->
+                    <!-- <a href="https://forge.laravel.com">Forge</a> -->
+                    <!-- <a href="https://vapor.laravel.com">Vapor</a> -->
+                    <!-- <a href="https://github.com/laravel/laravel">GitHub</a> -->
                 </div>
             </div>
         </div>
+
+        <!-- js -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </body>
 </html>
