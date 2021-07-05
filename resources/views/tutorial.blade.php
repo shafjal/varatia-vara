9fc51ed1dd7e0e83a83ebbfa6b0883eedc1b232b
shuvo <shuvoafjal@gmail.com>
2021-07-05 14:07:22 +0600

    Video Button Add

--- resources/views/tutorial.blade.php
+++ resources/views/tutorial.blade.php
@@ -8,6 +8,7 @@
               <div class="card-body">
                 <div class="embed-responsive embed-responsive-21by9">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/nRrD46ExEzs?rel=0" allowfullscreen></iframe>
+                    Your browser does not support the video tag.
                   </div>
               </div>
           </div>
@@ -30,18 +31,35 @@
             </div>
         </div>
     </div>
-      <div class="col-md-12 col-12 mt-5">
-        <div class="card">
-            <div class="card-header text-center"><h3><b> Download PDF</b></h3></div>
-            <div class="card-body text-center">
-              <a  href="{{url('/pdf/tutorial.pdf')}}">
-                <span style="font-size: 50px; color: rgb(255, 38, 0);">
-                  <i class="icofont-file-pdf"></i>
-                  </span>
-            </a>
-            <p>Tutorial</p>
-            </div>
+
+  </div>
+
+  <div class="row justify-content-center mt-5">
+    <div class="col-md-6 col-6">
+      <div class="card">
+          <div class="card-header text-center"><h3><b> Download PDF</b></h3></div>
+          <div class="card-body text-center">
+            <a  href="{{url('/pdf/tutorial.pdf')}}">
+              <span style="font-size: 50px; color: rgb(255, 38, 0);">
+                <i class="icofont-file-pdf"></i>
+                </span>
+          </a>
+          <p>Tutorial</p>
+          </div>
+      </div>
+  </div>
+  <div class="col-md-6 col-6">
+    <div class="card">
+        <div class="card-header text-center"><h3><b>Download Video</b></h3></div>
+        <div class="card-body text-center">
+          <a  href="{{url('/video/video.mp4')}}">
+            <span style="font-size: 50px; color: rgb(51, 26, 196);">
+              <i class="icofont-video-cam"></i>
+              </span>
+        </a>
+        <p>Video</p>
         </div>
     </div>
+</div>
   </div>
 </div>
