595bbafbff5fc8fc5c1e090cb63d747fb83aaea9
shuvo <shuvoafjal@gmail.com>
2021-07-19 21:37:44 +0600

    Blade Page- Profile Responsive Design Full Done

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -3,48 +3,47 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('My Profile') }}</div>
 
-                <div class="card-body text-center">
+                <div class="card-body text-center bg-warning">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
                         {{-- pic + Details --}}
-                        <div class="row justify-content-center">
-                            <div class="col-md-3 text-center">
-                                <img class="card-img-top " src="/avater.jpg" alt="Card image cap">
-                                <div class="col-md-1 col-lg-1 mt-3 ">
-                                    <button type="button" class="btn btn-success ">Upload Picture</button>
+                        <div class="row justify-content-center ">
+                            <div class="col-md-3 bg-secondary">
+                                <img class="card-img-top mt-2" src="/avater.jpg" alt="Card image cap">
+                                <div class="offset-md-2 col-md-1 col-lg-1 mt-3">
+                                    <button type="button" class="btn btn-success">Upload Picture</button>
                                 </div>    
                             </div>
-                            <div class="col-md-8 bg-info">
-                                    <table class="table">
-                                    <thead>
-                                        <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">First</th>
-                                        <th scope="col">Last</th>
-                                        <th scope="col">Handle</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                        </tr>
-                                        <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                        </tr>
-                                    </tbody>
+                            <div class="col-md-8 ">
+                                    <table class="table table-hover table-responsive-md  mt-5  table-bordered">
+                                        <thead class="bg-success">
+                                            <tr>
+                                                <th>First Name</th>
+                                                <th>Last Name</th>
+                                                <th>Email</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="bg-info">
+                                            <tr>
+                                                <td>Mark</td>
+                                                <td>Otto</td>
+                                                <td>shuvoafjal@gmail.com</td>
+                                                <td>
+                                                    <a href="" >
+                                                        <i class="icofont-edit" style="color: green"> Edit</i>
+                                                    </a>
+
+                                                </td>
+                                            </tr>
+                                        </tbody>
                                     </table>
                             </div>
                         </div>
