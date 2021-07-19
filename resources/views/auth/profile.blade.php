808de5ff9ed74f85d5b8c43b96aa25fa73ac61a5
shuvo <shuvoafjal@gmail.com>
2021-07-19 12:20:24 +0600

    Blade Page- Profile Responsive Design

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -5,21 +5,49 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('My Profile') }}</div>
 
-                <div class="card-body">
+                <div class="card-body text-center">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-                    @foreach ($admin as $user)
-                    <ul id="printableArea">
-                        <Li>Id = {{$user->id}}</Li>
-                        <li>Name = {{$user->name}}</li>
-                        <li>email = {{$user->email}}</li>
-                    </ul>
-                    @endforeach
+                        {{-- pic + Details --}}
+                        <div class="row justify-content-center">
+                            <div class="col-md-3 text-center">
+                                <img class="card-img-top " src="/avater.jpg" alt="Card image cap">
+                                <div class="col-md-1 col-lg-1 mt-3 ">
+                                    <button type="button" class="btn btn-success ">Upload Picture</button>
+                                </div>    
+                            </div>
+                            <div class="col-md-8 bg-info">
+                                    <table class="table">
+                                    <thead>
+                                        <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                        </tr>
+                                    </tbody>
+                                    </table>
+                            </div>
+                        </div>
                     {{-- {{ __('You are logged in!') }} --}}
                 </div>
             </div>
