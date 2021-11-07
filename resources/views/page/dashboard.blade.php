85a2a45cae1bf3240582a5840a14496975ae04de
shuvo <shuvoafjal@gmail.com>
2021-11-07 21:57:14 +0600

    Dashboard half

--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -4,10 +4,78 @@
 
 <div class="container-fluid px-4">
     <h1 class="mt-4 textColor">Dashboard</h1>
-    {{-- <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">Dashboard</li>
-    </ol> --}}
+    <hr style="width:100%;text-align:left;margin-left:0; border: 1px solid white;">
+    {{-- card section --}}
+    <div class="row">
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-success text-white mb-4 myShadow">
+                <div class="card-header">
+                    <i class="fa fa-money fa-2x" aria-hidden="true" style="float:left;">
 
+                    </i>
+                    <h5 class="text-right" style="margin-top: 1px">Current Balance</h5>
+                </div>
+                <div class="card-body">
+                    <h5 class="text-center">70,000/=</h5>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretched-link" href="#">View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-warning text-white mb-4 myShadow">
+                <div class="card-header">
+                    <i class="fa fa-money fa-2x" aria-hidden="true" style="float:left;">
+
+                    </i>
+                    <h5 class="text-right" style="margin-top: 1px">Current Due</h5>
+                </div>
+                <div class="card-body">
+                    <h5 class="text-center">70,000/=</h5>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretched-link" href="#">View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-danger text-white mb-4 myShadow">
+                <div class="card-header">
+                    <i class="fa fa-money fa-2x" aria-hidden="true" style="float:left;">
+
+                    </i>
+                    <h5 class="text-right" style="margin-top: 1px">Previous Due</h5>
+                </div>
+                <div class="card-body">
+                    <h5 class="text-center">70,000/=</h5>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretched-link" href="#">View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6">
+            <div class="card bg-info text-white mb-4 myShadow">
+                <div class="card-header">
+                    <i class="fa fa-money fa-2x" aria-hidden="true" style="float:left;">
+
+                    </i>
+                    <h5 class="text-right" style="margin-top: 1px">Estimet Cost</h5>
+                </div>
+                <div class="card-body">
+                    <h5 class="text-center">70,000/=</h5>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <a class="small text-white stretched-link" href="#">View Details</a>
+                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 {{-- <div class="container">
